ece0f6d9fbfdfa86993a8d93959cae1bda80b0d6
ian <ian.maina@student.moringaschool.com>
2023-07-30 18:16:42 +0300

    JSX working with no errors

--- src/components/About.js
+++ src/components/About.js
@@ -2,7 +2,11 @@ import React from "react";
 import { image } from "../data/data";
 
 function About() {
-  return <div>About</div>;
+  return <div id="about">
+    <h2>About Me</h2>
+    <p>Delivering the best Software solutions to Boost your Personal and Business Operations and online Presence</p>
+    <img src={image} alt="I made this"/>
+  </div>;
 }
 
 export default About;
